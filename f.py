fc627159cddf3818457b3decd1b78d968b390938
Edmilson-Dev-hub <edmilsonsantosg2@gmail.com>
2025-07-09 12:02:29 -0300

    f.py

---
+++ f.py
@@ -0,0 +1,12 @@
+#modo leitura ("r")
+# with open("C:/Users/User/Downloads/vendas.txt", "r") as f:
+#     texto = f.read()
+#     print(texto)
+
+#modo append("a") - adicionar no final
+# with open("C:/Users/User/Downloads/vendas.txt", "a") as f:
+#     f.write("\nlira, 5200")
+
+#modo criação("w")
+with open("C:/Users/User/Downloads/2026_vendas.txt", "w") as f:
+    f.write("\nlira, 5200")
